e36f34e0a76fbbd1926b32ed3fa10b44858af9aa
monster <worldoffantasy@livemail.tw>
2020-07-28 15:32:15 +0800

    feat: add data Model classes

---
+++ app/src/main/java/com/salab/project/projectbakingrecipe/Model/Ingredient.java
@@ -0,0 +1,32 @@
+package com.salab.project.projectbakingrecipe.Model;
+
+public class Ingredient {
+
+    private String ingredient;
+    private int quantity;
+    private String measure;
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+}
---
+++ app/src/main/java/com/salab/project/projectbakingrecipe/Model/Recipe.java
@@ -0,0 +1,61 @@
+package com.salab.project.projectbakingrecipe.Model;
+
+import java.util.List;
+
+public class Recipe {
+
+    private int id;
+    private String name;
+    private List<Ingredient> ingredients;
+    private List<RecipeStep> steps;
+    private int servings;
+    private String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<RecipeStep> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<RecipeStep> steps) {
+        this.steps = steps;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
