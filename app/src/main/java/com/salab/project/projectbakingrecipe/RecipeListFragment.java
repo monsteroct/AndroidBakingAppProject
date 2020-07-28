308afd0d515e92f62c5624e091ea23fa3164eacf
monster <worldoffantasy@livemail.tw>
2020-07-28 15:28:00 +0800

    feat: add RecipeListActivity, Fragment and their layout

---
+++ app/src/main/java/com/salab/project/projectbakingrecipe/RecipeListFragment.java
@@ -0,0 +1,28 @@
+package com.salab.project.projectbakingrecipe;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Fragment to display list of recipes
+ */
+public class RecipeListFragment extends Fragment {
+
+
+    public RecipeListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipe_list, container, false);
+    }
+}
