15348dc6faad0b651cc09ddea56a6763104b5f83
monster <worldoffantasy@livemail.tw>
2020-07-28 14:17:47 +0800

    init commit

---
+++ app/src/main/java/com/salab/project/projectbakingrecipe/RecipeListActivity.java
@@ -0,0 +1,14 @@
+package com.salab.project.projectbakingrecipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_list);
+    }
+}
