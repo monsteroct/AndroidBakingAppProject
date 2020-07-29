9aeaf587ebf04d324de242ccf652415982c6f226
monster <worldoffantasy@livemail.tw>
2020-07-29 11:13:56 +0800

    feat: add recipe step page

---
+++ app/src/main/java/com/salab/project/projectbakingrecipe/RecipeStepFragment.java
@@ -0,0 +1,49 @@
+package com.salab.project.projectbakingrecipe;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RecipeStepFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RecipeStepFragment extends Fragment {
+
+    private static final String ARG_RECIPE_ID = "recipe_argument";
+
+    private String mRecipeId;
+
+    public RecipeStepFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static RecipeStepFragment newInstance(String recipeId) {
+        RecipeStepFragment fragment = new RecipeStepFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_RECIPE_ID, recipeId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mRecipeId = getArguments().getString(ARG_RECIPE_ID);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipe_step, container, false);
+    }
+}
