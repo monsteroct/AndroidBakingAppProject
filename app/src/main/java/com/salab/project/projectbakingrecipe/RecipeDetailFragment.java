b38f0a72f45e67a5328636025e558d7f7e8c0077
monster <worldoffantasy@livemail.tw>
2020-07-29 08:50:37 +0800

    feat: add Adapter to back ingredient ListView

--- app/src/main/java/com/salab/project/projectbakingrecipe/RecipeDetailFragment.java
+++ app/src/main/java/com/salab/project/projectbakingrecipe/RecipeDetailFragment.java
@@ -1,14 +1,23 @@
 package com.salab.project.projectbakingrecipe;
 
+import android.app.IntentService;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import com.salab.project.projectbakingrecipe.Model.Ingredient;
 import com.salab.project.projectbakingrecipe.databinding.FragmentRecipeDetailBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Display recipe details including image, name, servings and ingredients
@@ -48,7 +57,80 @@ public class RecipeDetailFragment extends Fragment {
         // Inflate the layout for this fragment
         mBinding = FragmentRecipeDetailBinding.inflate(inflater, container, false);
 
+        // init ListView
+        IngredientListAdapter mAdapter = new IngredientListAdapter(getDummyDataSet());
+        mBinding.lvDetailIngredientList.setAdapter(mAdapter);
+        //TODO : fix the list view is scrollable
 
         return mBinding.getRoot();
+    }
+
+    private List<Ingredient> getDummyDataSet() {
+
+        List<Ingredient> ingredientList = new ArrayList<>();
+
+        Ingredient ingredientDummy = new Ingredient();
+        ingredientDummy.setIngredient("Graham Cracker crumbs");
+        ingredientDummy.setQuantity(2);
+        ingredientDummy.setMeasure("Cup");
+        ingredientList.add(ingredientDummy);
+
+        Ingredient ingredientDummy2 = new Ingredient();
+        ingredientDummy2.setIngredient("unsalted butter, melted");
+        ingredientDummy2.setQuantity(6);
+        ingredientDummy2.setMeasure("TBLSP");
+        ingredientList.add(ingredientDummy2);
+
+        return ingredientList;
+    }
+
+    /**
+     * Simple BaseAdapter is used to back the ingredients ListView
+     */
+    class IngredientListAdapter extends BaseAdapter{
+
+        private List<Ingredient> mIngredientList;
+
+        public IngredientListAdapter(List<Ingredient> IngredientList) {
+            mIngredientList = IngredientList;
+        }
+
+        @Override
+        public int getCount() {
+            if (mIngredientList == null) {
+                return 0;
+            } else {
+                return mIngredientList.size();
+            }
+        }
+
+        @Override
+        public Ingredient getItem(int position) {
+            return mIngredientList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null){
+                //inflate list item view once
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.ingredient_item, parent, false);
+            }
+
+            TextView ingredientNameTextView = convertView.findViewById(R.id.tv_item_ingredient_name);
+            TextView ingredientQuantityTextView = convertView.findViewById(R.id.tv_item_ingredient_quantity);
+            TextView ingredientMeasureTextView = convertView.findViewById(R.id.tv_item_ingredient_measure);
+
+            ingredientNameTextView.setText(getItem(position).getIngredient());
+            //getQuantity() returns integer
+            ingredientQuantityTextView.setText(String.valueOf(getItem(position).getQuantity()));
+            ingredientMeasureTextView.setText(getItem(position).getMeasure());
+
+            return convertView;
+        }
     }
 }
