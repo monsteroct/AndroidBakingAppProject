d0d593cb7a7394d565275f2c690e4f02f7732b17
monster <worldoffantasy@livemail.tw>
2020-07-30 20:20:37 +0800

    feat: add ExecutorStore

--- app/src/main/java/com/salab/project/projectbakingrecipe/ExecutorStore.java
+++ app/src/main/java/com/salab/project/projectbakingrecipe/ExecutorStore.java
@@ -1,4 +1,50 @@
 package com.salab.project.projectbakingrecipe;
 
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.annotation.NonNull;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * Hold Executors allowing easily switching between threads
+ */
 public class ExecutorStore {
+
+    private static ExecutorStore sInstance;
+    private static final Object LOCK = new Object();
+    Executor diskIO;
+    Executor networkIO;
+    Executor mainThread;
+
+    public ExecutorStore(Executor diskIO, Executor networkIO, Executor mainThread) {
+        this.diskIO = diskIO;
+        this.networkIO = networkIO;
+        this.mainThread = mainThread;
+    }
+
+    public static ExecutorStore getInstance(){
+        // For Singleton instantiation
+        if (sInstance == null){
+            synchronized (LOCK){
+                sInstance = new ExecutorStore(
+                        Executors.newSingleThreadExecutor(),
+                        Executors.newSingleThreadExecutor(),
+                        new MainThreadExecutor());
+            }
+        }
+        return sInstance;
+    }
+
+    private static class MainThreadExecutor implements Executor {
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(@NonNull Runnable command) {
+            mainThreadHandler.post(command);
+        }
+    }
+
 }
