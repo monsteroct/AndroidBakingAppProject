0aa43fd72ba906ff6f63a67555d2ceafcd74fedd
monster <worldoffantasy@livemail.tw>
2020-07-30 15:30:43 +0800

    feat: change recipe step master-detail flow from two activities into one

--- app/src/main/java/com/salab/project/projectbakingrecipe/RecipeActivity.java
+++ app/src/main/java/com/salab/project/projectbakingrecipe/RecipeActivity.java
@@ -7,17 +7,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-public class RecipeActivity extends AppCompatActivity {
+import static com.salab.project.projectbakingrecipe.RecipeStepActivity.EXTRA_RECIPE_ID;
+import static com.salab.project.projectbakingrecipe.RecipeStepActivity.EXTRA_STEP_ID;
+
+public class RecipeActivity extends AppCompatActivity  implements RecipeStepListAdapter.RecipeStepOnClickListener{
 
     public static final String EXTRA_RECIPE_ID = "selected_recipe_id";
     private static final String TAG = RecipeActivity.class.getSimpleName();
 
     private int mRecipeId = -1;
+    private boolean mTwoPane;
+
+    private FragmentManager mFragmentManager = getSupportFragmentManager();
+    // single Fragment instance for each class, relying on ViewModel to update data inside
+    private RecipeDetailFragment recipeDetailFragment;
+    private RecipeStepFragment recipeStepFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        if (findViewById(R.id.container_recipe_step_detail) != null){
+            mTwoPane = true;
+            Log.d(TAG, "Recipe detail page in two pane mode");
+        }
 
         Intent receivedIntent = getIntent();
         if (receivedIntent != null && receivedIntent.hasExtra(EXTRA_RECIPE_ID)){
@@ -27,23 +41,44 @@ public class RecipeActivity extends AppCompatActivity {
             Log.d(TAG, "invalid start");
         }
 
-        if (mRecipeId != -1){
+        if (mRecipeId != -1 && savedInstanceState == null){
+            // only create new instances of fragments when fresh activity created (not configuration change)
             initFragments();
-        } else {
-            Log.d(TAG, "invalid recipe id");
+        }
+    }
+
+    private void initFragments() {
+        // Create corresponding fragments to this recipe
+        recipeDetailFragment = RecipeDetailFragment.newInstance(mRecipeId);
+        recipeStepFragment = RecipeStepFragment.newInstance(mRecipeId, 1);
+
+        // one pane mode all share the same container
+        mFragmentManager.beginTransaction()
+                .add(R.id.container_recipe_detail, recipeDetailFragment)
+                .commit();
+
+        if (mTwoPane) {
+            // two pane mode has another container.
+            mFragmentManager.beginTransaction()
+                    .add(R.id.container_recipe_step_detail, recipeStepFragment)
+                    .commit();
         }
 
     }
 
-    private void initFragments() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        // Create corresponding fragments to this recipe
-        RecipeDetailFragment recipeDetailFragment = RecipeDetailFragment.newInstance(mRecipeId);
-        RecipeStepListFragment recipeStepListFragment = RecipeStepListFragment.newInstance(mRecipeId);
-        // fill into containers
-        fragmentManager.beginTransaction()
-                .replace(R.id.container_recipe_detail, recipeDetailFragment)
-                .replace(R.id.container_step_list, recipeStepListFragment)
+    @Override
+    public void onRecipeStepClick(int stepId) {
+        // decide step detail fragment should fill into which container
+        int containerId;
+
+        if (mTwoPane){
+            containerId = R.id.container_recipe_step_detail;
+        } else {
+            containerId = R.id.container_recipe_detail;
+        }
+
+        mFragmentManager.beginTransaction()
+                .replace(containerId, recipeStepFragment)
                 .commit();
     }
 }
