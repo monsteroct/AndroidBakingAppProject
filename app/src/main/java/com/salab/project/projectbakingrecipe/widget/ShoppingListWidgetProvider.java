7e173d698fc328d032e9c44f6bd6fbee11e4b7c6
monster <worldoffantasy@livemail.tw>
2020-08-02 15:29:05 +0800

    fix: cleanup and reorganize code structure in AppWidgetProvider

--- app/src/main/java/com/salab/project/projectbakingrecipe/widget/ShoppingListWidgetProvider.java
+++ app/src/main/java/com/salab/project/projectbakingrecipe/widget/ShoppingListWidgetProvider.java
@@ -29,55 +29,29 @@ public class ShoppingListWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId, Recipe selectedRecipe) {
 
-        boolean isEmpty = selectedRecipe == null;
+        boolean hasSelectedRecipe = selectedRecipe == null;
 
-        // TODO : reorganize logic here
-        // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.shopping_list);
-        if (!isEmpty) {
-            //leave empty when there's no favorite recipe
+        // Setup list view adapter
+        views.setRemoteAdapter(R.id.lv_widget_ingredient_list, new Intent(context, ShoppingListRemoteViewsServices.class));
+        views.setPendingIntentTemplate(R.id.lv_widget_ingredient_list, getItemCheckPendingIntent(context));
+        views.setEmptyView(R.id.lv_widget_ingredient_list, R.id.tv_widget_empty_view);
+
+        if (!hasSelectedRecipe) {
             views.setTextViewText(R.id.tv_widget_recipe_name, selectedRecipe.getName());
             views.setTextViewText(R.id.tv_widget_recipe_servings, String.valueOf(selectedRecipe.getServings()));
 
             // On recipe title click, redirect to the recipe page
-            Intent recipeDetailIntent = new Intent(context, RecipeActivity.class);
-            recipeDetailIntent.putExtra(EXTRA_RECIPE_ID, selectedRecipe.getId());
-            PendingIntent recipeDetailPendingIntent = PendingIntent.getActivity(context,
-                    RECIPE_DETAIL_REQUEST_CODE,
-                    recipeDetailIntent,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
-
-            views.setOnClickPendingIntent(R.id.tv_widget_recipe_name, recipeDetailPendingIntent);
+            views.setOnClickPendingIntent(R.id.tv_widget_recipe_name, getRecipeDetailPendingIntent(context, selectedRecipe));
 
         } else {
+            // override title with empty string when there's no favorite recipe
             views.setTextViewText(R.id.tv_widget_recipe_name, "");
             views.setTextViewText(R.id.tv_widget_recipe_servings, "");
+            // when there's no favorite recipe. show alternative view which redirecting to recipe list paget
+            views.setOnClickPendingIntent(R.id.tv_widget_empty_view, getRecipeListPendingIntent(context));
         }
 
-        Intent itemCheckIntent = new Intent(context, ShoppingListUpdateService.class);
-        itemCheckIntent.setAction(ACTION_INGREDIENT_CHECKED);
-        PendingIntent itemCheckPendingIntent = PendingIntent.getService(context,
-                ITEM_CHECK_REQUEST_CODE,
-                itemCheckIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-
-        views.setRemoteAdapter(R.id.lv_widget_ingredient_list, new Intent(context, ShoppingListRemoteViewsServices.class));
-        views.setPendingIntentTemplate(R.id.lv_widget_ingredient_list, itemCheckPendingIntent);
-        // alternative view when there's no favorite recipe
-        views.setEmptyView(R.id.lv_widget_ingredient_list, R.id.tv_widget_empty_view);
-
-
-        if (isEmpty){
-            // when there's no favorite recipe. click alternative view will redirect to recipe list activity
-            Intent recipeListIntent = new Intent(context, RecipeListActivity.class);
-            PendingIntent recipeListPendingIntent = PendingIntent.getActivity(context,
-                    RECIPE_LIST_REQUEST_CODE,
-                    recipeListIntent,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
-            views.setOnClickPendingIntent(R.id.tv_widget_empty_view, recipeListPendingIntent);
-        }
-
-        // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
         Log.d(TAG, "widget no: " + appWidgetId + " updated");
     }
@@ -88,14 +62,37 @@ public class ShoppingListWidgetProvider extends AppWidgetProvider {
         }
     }
 
+    private static PendingIntent getRecipeListPendingIntent(Context context) {
+        Intent recipeListIntent = new Intent(context, RecipeListActivity.class);
+        return PendingIntent.getActivity(context,
+                RECIPE_LIST_REQUEST_CODE,
+                recipeListIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private static PendingIntent getItemCheckPendingIntent(Context context) {
+        Intent itemCheckIntent = new Intent(context, ShoppingListUpdateService.class);
+        itemCheckIntent.setAction(ACTION_INGREDIENT_CHECKED);
+        return PendingIntent.getService(context,
+                ITEM_CHECK_REQUEST_CODE,
+                itemCheckIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private static PendingIntent getRecipeDetailPendingIntent(Context context, Recipe selectedRecipe) {
+        Intent recipeDetailIntent = new Intent(context, RecipeActivity.class);
+        recipeDetailIntent.putExtra(EXTRA_RECIPE_ID, selectedRecipe.getId());
+        return PendingIntent.getActivity(context,
+                RECIPE_DETAIL_REQUEST_CODE,
+                recipeDetailIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-        Log.d(TAG, "on update called");
         ShoppingListUpdateService.startContentUpdateService(context);
-
-        // !! test force set a favorite recipe
-        ShoppingListUpdateService.startChangeRecipeService(context, 1);
     }
 
     @Override
